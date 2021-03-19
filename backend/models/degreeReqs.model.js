bcf27b7ef77d905f1f26b49c90f79ffcc7a16a3e
Anthony Li <antli@cs.stonybrook.edu>
2021-03-18 21:58:38 -0400

    created schema's for models, added to routes files

--- backend/models/degreeReqs.model.js
+++ backend/models/degreeReqs.model.js
@@ -3,18 +3,29 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  username: {
+const degreeSchema = new Schema({
+  department: {
     type: String,
     required: true,
-    unique: true,
+    // unique: true,
     trim: true,
-    minlength: 3
+    // minlength: 3
   },
+  gpaReq: { type: Number, required: true, },
+  track: { type: String, required: true, },
+  reqVersionSem: { type: String, required: true, },
+  reqVersionYear: { type: String, required: true, },
+  courseReqs: { type: Map, required: true, },
+  timeLimit: { type: Number, required: true, },
+  creditEq: { type: Number, required: true, },
+  thesisOption: { type: Boolean, required: true, },
+  noThesisOption: { type: Boolean, required: true, },
+  projectOption: { type: Boolean, required: true, },
 }, {
   timestamps: true,
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model('Degree', degreeSchema);
 
 module.exports = User;
+module.exports.degreeSchema = degreeSchema;
---
+++ backend/models/student.model.js
@@ -0,0 +1,37 @@
+//SAMPLE MODEL
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const degreeReqs = require('./degreeReqs.model');
+
+const schema = degreeReqs.degreeSchema
+
+const studentSchema = new Schema({
+  first_name: { type: String, required: true, minLength: 1},
+  last_name: { type: String, required: true, minLength: 1 },
+  sbu_id: { type: Number, required: true, length: 9 }, //TODO might work?
+  email: { type: String, required: true },
+  gpa: { type: Number, required: true },
+  department: { type: String, required: true },
+  track: { type: String, required: true },
+  reqVersion: { type: String, required: true },
+  entrySem: { type: String, required: true },
+  entryYear: { type: String, required: true },
+  gradSem: { type: String, required: true },
+  coursePlan: { type: String, required: true }, //TODO
+  projectOption: { type: String, required: true },
+  faculty_advisor: { type: String, required: true },
+  proficiencyReq: { type: Array, required: true },
+  reqVersion: { type: schema, required: true }, //TODO CHECK
+  password: { type: String, required: true },
+  graduated: { type: Boolean, required: true },
+  settings: { type: String, required: true }, //TODO
+  comments: { type: Array, required: true }, //TODO
+}, {
+  timestamps: true,
+});
+
+const Exercise = mongoose.model('Student', studentSchema);
+
+module.exports = Exercise;
