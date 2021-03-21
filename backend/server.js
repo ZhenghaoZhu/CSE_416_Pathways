fa5995306243473399ec57e6d9e0373e10ab5ad2
Zhenghao (Kevin) Zhu <kevinzhuzhenghao@gmail.com>
2021-03-20 21:54:52 -0400

    Changes to schema

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,17 +10,23 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
-const connection = mongoose.connection;connection.once('open', () => {
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+const connection = mongoose.connection;
+connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
-})
+});
 
-const coursesRouter = require('./routes/courses');
-const studentsRouter = require('./routes/students');
+const coursesRouter = require("./routes/courses");
+const studentsRouter = require("./routes/student");
 
-app.use('/courses', coursesRouter);
-app.use('/students', studentsRouter);
+app.use("/courses", coursesRouter);
+app.use("/student", studentsRouter);
 
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+  console.log(`Server is running on port: ${port}`);
 });
