2a03a795f718cba923edc8e8fa6722e2e87dfbc3
1240831308 <56331824+1240831308@users.noreply.github.com>
2021-03-22 14:47:07 -0400

    GPD Header and Student Detail/Table (#20)

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,21 +1,38 @@
 import "../style/App.css";
-
+import React from "react";
+import GPDHeader from "./GPDHeader";
+import StudentTable from "./StudentTable";
+import StudentDetail from "./StudentDetail";
+import { DropzoneArea } from "material-ui-dropzone";
+import { Grid, Button, ButtonGroup } from "@material-ui/core";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <GPDHeader />
+      <Grid container spacing={1}>
+        <Grid item xs={8}>
+          <StudentTable />
+          <Button 
+            variant="contained"
+            color="primary">
+            Add Student
+          </Button>
+        </Grid>
+
+        <Grid item xs={4}>
+          <StudentDetail />
+          <ButtonGroup
+            variant="contained"
+            color="primary"
+            aria-label="contained primary button group"
+          >
+            <Button>Edit Student</Button>
+            <Button>Suggest Course Plan</Button>
+            <Button>Make Comment</Button>
+          </ButtonGroup>
+          <DropzoneArea onChange={(files) => console.log("Files:", files)} />
+        </Grid>
+      </Grid>
     </div>
   );
 }
