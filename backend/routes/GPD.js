3ed872263259f695d06475c620dd8ac8ffb08228
Zhenghao (Kevin) Zhu <kevinzhuzhenghao@gmail.com>
2021-03-24 22:45:10 -0400

    Added authentication

--- backend/routes/GPD.js
+++ backend/routes/GPD.js
@@ -1,29 +1,37 @@
 //SAMPLE endpoints
-const router = require('express').Router();
-let GPD = require('../models/GPD.model');
+const router = require("express").Router();
+let GPD = require("../models/GPD.model");
 
-router.route('/').get((req, res) => {
+router.route("/").get((req, res) => {
   GPD.find()
-    .then(users => res.json(users))
-    .catch(err => res.status(400).json('Error: ' + err));
+    .then((users) => res.json(users))
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
-router.route('/add').post((req, res) => {
+router.route("/add").post((req, res) => {
   const firstName = req.body.firstName;
   const lastName = req.body.lastName;
   const sbuID = req.body.sbuID;
   const department = req.body.department;
   const pageSetting = req.body.pageSetting;
+  const email = req.body.email;
+  const password = req.body.password;
+  const newUser = new GPD({
+    firstName,
+    lastName,
+    sbuID,
+    department,
+    pageSetting,
+    email,
+    password,
+  });
 
-  const newUser = new GPD({firstName, lastName, sbuID, department, pageSetting});
-
-  newUser.save()
-    .then(() => res.json('GPD added!'))
-    .catch(err => res.status(400).json('Error: ' + err));
+  newUser
+    .save()
+    .then(() => res.json("GPD added!"))
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
-router.route("/get").get((req, res) => {
-
-})
+router.route("/get").get((req, res) => {});
 
 module.exports = router;
