605c44cb1c1b2d63feb924f0bebc4c05eba88d59
Zhenghao (Kevin) Zhu <kevinzhuzhenghao@gmail.com>
2021-03-31 21:13:46 -0400

    Fixed add student form (#77)

--- backend/models/courses.model.js
+++ backend/models/courses.model.js
@@ -5,14 +5,17 @@ const Schema = mongoose.Schema;
 const coursesSchema = new Schema(
   {
     id: { type: String, required: true, minLength: 1 },
+    // 220
     courseName: { type: String, required: true, minLength: 1 },
-    courseIden: { type: String, required: true, minLength: 1 },
+    // Sys Fundamentals I
     department: { type: String, required: true, minLength: 1 },
+    // CSE
     credits: { type: Number, required: true },
     preReqs: { type: String, required: true, minLength: 1 },
     courseDescription: { type: String, required: true, minLength: 1 },
     yearTrends: { type: Map, required: true },
-    timeSlots: { type: Map, required: true },
+    courseInfo: { type: Map, required: true },
+    // {"Spring 2021" : [1, "MW 10:30pm - 11:30pm"], "Spring 2021" : [2, "TuTh 10:30pm - 11:30pm"]}
     professorNames: { type: Map, required: true },
   },
   {
