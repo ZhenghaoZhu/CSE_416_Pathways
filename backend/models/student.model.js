ea69402aaee0a4aac5ee3979ef62ee2b254f84cb
Zhenghao (Kevin) Zhu <kevinzhuzhenghao@gmail.com>
2021-04-04 01:50:39 -0400

    Degree reqs db change (#87)
    * Changes to degree reqs schema
    
    * Fixed add student

--- backend/models/student.model.js
+++ backend/models/student.model.js
@@ -8,38 +8,38 @@ const degreeReqs = require("./degreeReqs.model");
 const degreeReqsSchema = degreeReqs.degreeSchema;
 
 const studentSchema = new Schema(
-  {
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    id: { type: Number, required: true },
-    email: { type: String, required: true },
-    gpa: { type: Number, required: true },
-    department: { type: String, required: true },
-    track: { type: String, required: true },
-    reqVersionSem: { type: String, required: true },
-    reqVersionYear: { type: String, required: true },
-    entrySem: { type: String, required: true },
-    entryYear: { type: String, required: true },
-    gradSem: { type: String, required: true },
-    gradYear: { type: String, required: true },
-    coursePlan: { type: Map, required: true },
-    // {"Spring 2020": [Course1, Course2, ...], "Fall 2020": [Course1, Course2, ...], "Spring 2021": [Course1, Course2, ...], "Fall 2021": [Course1, Course2, ...]}
-    projectOption: { type: String, required: true },
-    facultyAdvisor: { type: String, required: true },
-    proficiencyReq: { type: Array, required: true },
-    degreeRequirements: { type: String, required: true }, //TODO CHECK
-    curSem: { type: String, required: true },
-    curYear: { type: String, required: true },
-    password: { type: String, required: true },
-    graduated: { type: Boolean, required: true },
-    settings: { type: String, required: true },
-    comments: { type: Array, required: true },
-  },
-  {
-    timestamps: true,
-  }
+    {
+        firstName: { type: String, required: true },
+        lastName: { type: String, required: true },
+        id: { type: Number, required: true },
+        email: { type: String, required: true },
+        gpa: { type: Number, required: true },
+        department: { type: String, required: true },
+        track: { type: String, required: true },
+        reqVersionSem: { type: String, required: true },
+        reqVersionYear: { type: String, required: true },
+        entrySem: { type: String, required: true },
+        entryYear: { type: String, required: true },
+        gradSem: { type: String, required: true },
+        gradYear: { type: String, required: true },
+        coursePlan: { type: Map, required: true },
+        // {"Spring 2020": [Course1, Course2, ...], "Fall 2020": [Course1, Course2, ...], "Spring 2021": [Course1, Course2, ...], "Fall 2021": [Course1, Course2, ...]}
+        projectOption: { type: String, required: true },
+        facultyAdvisor: { type: String, required: true },
+        proficiencyReq: { type: Array, required: true },
+        degreeRequirements: { type: String, required: true }, //TODO CHECK
+        curSem: { type: String, required: true },
+        curYear: { type: String, required: true },
+        password: { type: String, required: true },
+        graduated: { type: Boolean, required: true },
+        settings: { type: String, required: true },
+        comments: { type: Array, required: true },
+    },
+    {
+        timestamps: true,
+    }
 );
 
-const Exercise = mongoose.model("Student", studentSchema);
+const Student = mongoose.model("Student", studentSchema);
 
-module.exports = Exercise;
+module.exports = Student;
--- backend/routes/degreeReqs.js
+++ backend/routes/degreeReqs.js
@@ -1,62 +1,75 @@
 //SAMPLE endpoints
 
+const router = require("express").Router();
+let Degree = require("../models/degreeReqs.model");
 
-const router = require('express').Router();
-let User = require('../models/degreeReqs.model');
-
-router.route('/').get((req, res) => {
-  User.find()
-    .then(users => res.json(users))
-    .catch(err => res.status(400).json('Error: ' + err));
+router.route("/").get((req, res) => {
+    Degree.find()
+        .then((allReqs) => res.json(allReqs))
+        .catch((err) => res.status(400).json("Error: " + err));
 });
+
+// router.route("/add").post(req, res) => {
+// 	const department = req.body.department
+// 	const gpaReq =
+// 	const track =
+// 	const reqVersionSem =
+// 	const reqVersionYear =
+// 	const courseReqs =
+// 	const timeLimit =
+// 	const creditEq =
+// 	const thesisOption =
+// 	const noThesisOption =
+// 	const projectOption =
+// }
 
 router.route("/get/:id").get((req, res) => {
-  User.findById(req.params.id)
-    .then((curStudent) => res.json(curStudent))
-    .catch((err) => res.status(400).json("Error: " + err));
+    Degree.findById(req.params.id)
+        .then((curReqs) => res.json(curReqs))
+        .catch((err) => res.status(400).json("Error: " + err));
 });
 
-router.route("/remove/:id").delete(function(req, res) { 
-  User.findByIdAndDelete(req.params.id, function (err, docs) { 
-    if (err){ 
-        res.send("Remove ID not work")
-    } 
-    else{ 
-        res.json(docs);
-    } 
-  }); 
+router.route("/remove/:id").delete(function (req, res) {
+    Degree.findByIdAndDelete(req.params.id, function (err, docs) {
+        if (err) {
+            res.send("Remove ID not work");
+        } else {
+            res.json(docs);
+        }
+    });
 });
 
-router.route('/add').post((req, res) => {
-  const department = req.body.department;
-  const gpaReq = req.body.gpaReq;
-  const track = req.body.track;
-  const reqVersionSem = req.body.reqVersionSem;
-  const reqVersionYear = req.body.reqVersionYear;
-  const courseReqs = req.body.courseReqs;
-  const timeLimit = req.body.timeLimit;
-  const creditEq = req.body.creditEq;
-  const thesisOption = req.body.thesisOption;
-  const noThesisOption = req.body.noThesisOption;
-  const projectOption = req.body.projectOption;
+router.route("/add").post((req, res) => {
+    const department = req.body.department;
+    const gpaReq = req.body.gpaReq;
+    const track = req.body.track;
+    const reqVersionSem = req.body.reqVersionSem;
+    const reqVersionYear = req.body.reqVersionYear;
+    const courseReqs = req.body.courseReqs;
+    const timeLimit = req.body.timeLimit;
+    const creditEq = req.body.creditEq;
+    const thesisOption = req.body.thesisOption;
+    const noThesisOption = req.body.noThesisOption;
+    const projectOption = req.body.projectOption;
 
-  const newUser = new User({
-    department,
-    gpaReq,
-    track,
-    reqVersionSem,
-    reqVersionYear,
-    courseReqs,
-    timeLimit,
-    creditEq,
-    thesisOption,
-    noThesisOption,
-    projectOption,
-  });
+    const newReqs = new User({
+        department,
+        gpaReq,
+        track,
+        reqVersionSem,
+        reqVersionYear,
+        courseReqs,
+        timeLimit,
+        creditEq,
+        thesisOption,
+        noThesisOption,
+        projectOption,
+    });
 
-  newUser.save()
-    .then(() => res.json('Degree Reqs added!'))
-    .catch(err => res.status(400).json('Error: ' + err));
+    newReqs
+        .save()
+        .then(() => res.json("Degree Reqs added!"))
+        .catch((err) => res.status(400).json("Error: " + err));
 });
 
 module.exports = router;
