28065f973b5bd41dc66671e556a3eb42e6023fd4
Zhenghao (Kevin) Zhu <kevinzhuzhenghao@gmail.com>
2021-04-05 21:52:37 -0400

    Suggest course plan (#95)
    * Added JSON again
    
    * Schemas for requirements

--- backend/models/amsDegree.model.js
+++ backend/models/amsDegree.model.js
@@ -3,25 +3,20 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const degreeSchema = new Schema(
+const amsDegreeSchema = new Schema(
     {
         department: { type: String, required: true },
         gpaReq: { type: Number, required: true },
         reqVersionSem: { type: String, required: true },
         reqVersionYear: { type: String, required: true },
-        courseReqs: { type: Map, required: true },
         timeLimit: { type: Number, required: true },
-        creditEq: { type: Number, required: true },
-        tracks: { type: Map, required: true},
-        thesisOption: { type: Boolean, required: true },
-        noThesisOption: { type: Boolean, required: true },
-        projectOption: { type: Boolean, required: true },
+        tracks: { type: Map, required: true },
     },
     {
         timestamps: true,
     }
 );
 
-const DegreeReqs = mongoose.model("Degree", degreeSchema);
+const DegreeReqs = mongoose.model("amsDegreeSchema", amsDegreeSchema);
 
 module.exports = DegreeReqs;
