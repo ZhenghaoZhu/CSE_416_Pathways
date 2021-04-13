3b5d95972cb180afddb00cfe0daad9863044e3bb
Anthony Li <antli@cs.stonybrook.edu>
2021-04-13 15:00:48 -0400

    Degree req routes (#104)
    * routes for ams, ece, and bmi
    
    * removed degree stuff from degreeReqs
    
    * changed jsons, worked on importing deg reqs
    
    * adding deg reqs works for ams, ece, and bmi
    
    * used heroku url instead of local

--- backend/routes/degreeReqs.js
+++ backend/routes/degreeReqs.js
@@ -4,6 +4,8 @@ const router = require("express").Router();
 let ams = require("../models/amsDegree.model");
 let bmi = require("../models/BMI_degreeReqs.model");
 let ece = require("../models/CE_degreeReqs.model")
+const fs = require("fs")
+
 
 router.route("/get/AMS/:year/:sem").get((req, res) => {
     ams.findOne({department: "AMS", reqVersionSem: req.params.sem, reqVersionYear: req.params.year})
@@ -56,20 +58,18 @@ router.route("/add/ECE").post((req, res) => {
     const gpaReq = req.body.gpaReq;
     const reqVersionSem = req.body.reqVersionSem;
     const reqVersionYear = req.body.reqVersionYear;
-    const requiredCourseReqs = req.body.requiredCourseReqs;
-    const electiveCourseReqs = req.body.electiveCourseReqs;
     const timeLimit = req.body.timeLimit;
     const thesisOption = req.body.thesisOption;
+    const tracks = req.body.tracks;
 
     const newReqs = new ece({
         department,
         gpaReq,
         reqVersionSem,
         reqVersionYear,
-        requiredCourseReqs,
-        electiveCourseReqs,
         timeLimit,
         thesisOption,
+        tracks,
     });
 
     newReqs.save()
@@ -80,23 +80,17 @@ router.route("/add/ECE").post((req, res) => {
 router.route("/add/BMI").post((req, res) => {
     const department = req.body.department;
     const gpaReq = req.body.gpaReq;
-    const specialization = req.body.specialization;
     const tracks = req.body.tracks;
     const reqVersionSem = req.body.reqVersionSem;
     const reqVersionYear = req.body.reqVersionYear;
-    const requiredCourseReqs = req.body.requiredCourseReqs;
-    const electiveCourseReqs = req.body.electiveCourseReqs;
     const timeLimit = req.body.timeLimit;
 
     const newReqs = new bmi({
         department,
         gpaReq,
-        specialization,
         tracks,
         reqVersionSem,
         reqVersionYear,
-        requiredCourseReqs,
-        electiveCourseReqs,
         timeLimit,
     });
 
@@ -108,5 +102,16 @@ router.route("/add/BMI").post((req, res) => {
 // router.route("/add/CSE").post((req, res) => {
     
 // })
+
+router.route("/file/:file").get((req,res) =>{
+    fs.readFile("degreeRequirements/" + req.params.file, "utf8", (err, string) => {
+        if(err){
+            return err;
+        }
+        res.json(string);
+        // var customer = JSON.parse(string);
+        // console.log("degree req: ", customer);
+    });
+})
 
 module.exports = router;
