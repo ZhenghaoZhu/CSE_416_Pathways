484512a6cd326c39d9191e60b723f6c69d8fca5d
Anthony Li <antli@cs.stonybrook.edu>
2021-05-02 11:07:55 -0400

    fixed retrieve course issues

--- client/src/components/suggestCP.js
+++ client/src/components/suggestCP.js
@@ -9,8 +9,7 @@ const axios = require("axios").default;
 class coursePlan extends Component {
     constructor(props) {
         super(props);
-        // GetStudent();
-        // this.student = location.state.student;
+
         this.student = this.props.location.student["row"];
         this.courseP = new Map(); //course plans
         // {"Spring 2021": [[1,"AMS 310", "MW 10"],[1,"CSE 320", "MW 12"]]}
@@ -36,16 +35,28 @@ class coursePlan extends Component {
         this.coursesAdded = 0;
         this.maxCoursesAllowed = 4; //default is 4
         this.times = new Map(); //{"Fall 2020": [["MW 10:30..."],["F 10:30"]]}
-        this.degReqs = this.student["degreeRequirements"]["Tracks"][this.student["track"]];
+        this.degReqs = this.student["degreeRequirements"]["tracks"][this.student["track"]];
+        //var to hold courses student need to take every sem
+        this.takeEverySem = [];
+        for(var i = 0; i < this.degReqs["Required Courses"].size; i++){
+            if(this.degReqs["Required Courses"][i][0] == -2){
+                this.takeEverySem.push(this.degReqs["Required Courses"][i][1]);//add the class we need
+                //to take every semester to this.takeEverySem
+            }
+        }
+        console.log("takeEverySem: ", this.takeEverySem);
     }
 
     smartCoursePlan = () => {
-        this.obtainStudents();
+        
+        this.obtainStudentsAndSort();
     };
 
-    coursePlanConstraints() {}
+    coursePlanConstraints() {
+        
+    }
 
-    obtainStudents = async function () {
+    obtainStudentsAndSort = async function () {
         var jsonT;
         //retrieve students that match department and track criteria
         await axios
@@ -97,6 +108,7 @@ class coursePlan extends Component {
         console.log(keysSorted); //sorts with most similar in front
         this.calculateClasses(keysSorted, jsonT["data"], map);
     };
+
     //keysSorted = array with indexes from most similar to least similar
     //data = javascript object with other student's data (same dep and same track and graduated)
     calculateClasses(keysSorted, data, studentMap) {
@@ -125,17 +137,23 @@ class coursePlan extends Component {
             yield* [...this.entries()].sort((a, b) => b[1] - a[1]);
         }
         //TODO may need to loop til degree reqs are good
+        
         for (let [key, value] of classesMap) {     // get data in descending (large -> small) sorted
             console.log(key + ' ' + value);
-            // var addCourseRet = this.addCourse(key, studentMap);
-            // if(addCourseRet === " "){
-            //     break;
-            // }
+            var addCourseRet = this.addCourse(key, studentMap);
+            if(addCourseRet === " "){
+                break;
+            }
         }
+        // var star = this.nextSemester();
+        // if(star === " "){
+        //     return " ";
+        // }
+        //TODO after loop, we need to display the plan somehow
         console.log("sorted list of classes by occurrences: ", classesMap);
     }
     //key = class name like "AMS 310"
-    //studentMap = //map to store cur student's courses taken
+    //studentMap = map to store cur student's courses taken
     addCourse(key, studentMap) {
         //check pre-reqs and time constraints
         if (studentMap[key] !== undefined) {
@@ -155,44 +173,84 @@ class coursePlan extends Component {
             this.courseP[this.sem+" "+this.year] = []
         }
         //check for prereqs and time constraints here **
-        var courseInfo = this.retrieveCourseInfo(studentMap[key]); //gets the course info from db
-        if(courseInfo === null){
+        console.log("key: ", key);
+        var courseInfo = this.retrieveCourseInfo(key); //gets the course info from db
+        if(courseInfo === null || courseInfo["data"] === undefined){
+            console.log("No courses found1111");
             return;
         }
+        console.log("retrieve course: ", courseInfo["data"]);
         //TODO CHeck for no classes returned from courseinfo
-        var status = this.meetPrereq(courseInfo[0], studentMap);
-        if(status){ //if pre req is not met, do not attempt to add course
-            return; 
-        }
-        var timeTF = this.meetTimeReq(courseInfo[0]); //pass in map 
-        //returns the time that works with user
-        if(timeTF === ""){//time contraints not met
-            return;
-        }
-        //now we want to go through the degReqs and remove from it
-        this.removeFromDegReqsList(courseInfo[0]);
-        //increment the course count
-        this.coursesAdded += 1;
-        this.courseP.get(this.sem+" "+this.year).push([1, key, timeTF]);
-        //after a class is added, check if deg reqs are done
-        if (this.degreeReqsSatisfied()) {
-            //return a course plan ()
-            return " ";
-        }
+        // var status = this.meetPrereq(courseInfo["data"][0], studentMap);
+        // if(status){ //if pre req is not met, do not attempt to add course
+        //     return; 
+        // }
+        // var timeTF = this.meetTimeReq(courseInfo[0]); //pass in map 
+        // //returns the time that works with user
+        // if(timeTF === ""){//time contraints not met
+        //     return;
+        // }
+        // //now we want to go through the degReqs and remove from it
+        // this.removeFromDegReqsList(courseInfo[0]);
+        // //increment the course count
+        // this.coursesAdded += 1;
+        // this.courseP.get(this.sem+" "+this.year).push([1, key, timeTF]);
+        // //after a class is added, check if deg reqs are done
+        // if (this.degreeReqsSatisfied()) {
+        //     //return a course plan ()
+        //     return " ";
+        // }
     }
     //look at the deg req list and remove from it
+    //courseInfoMap = course object
     removeFromDegReqsList(courseInfoMap){
         //TODO might need to handle checking for excluded courses
+
         var flag = 0; //flag to see if course is in required courses
         //if not, check elective courses
         var reqC = this.degReqs["Required Courses"];
         for(var i = 0; i < reqC.length; i++){
             //check if course name + number matches
-            for(var j = 1; j < reqC[i].length; j++){
+            for(var j = 1; j < reqC[i].length; j++){ //[1, "MCB 520", "CHE 541"],
+                if(reqC[i][j].substring(0,3) !== courseInfoMap["department"]){
+                    continue;
+                }
+                if(reqC[i][j].includes("-")){
+                    var str = reqC[i][j].match("(\d{3})\b\s*-\s*\b(\d{3})");
+                    if(str == null){
+                        continue;
+                    }
+                    var match1 = parseInt(str[1]); //get lower class #
+                    var match2 = parseInt(str[2]); //get higher class #
+                    var courseNum = parseInt(courseInfoMap["courseNum"]);
+                    if(courseNum > match1 && courseNum < match2){
+                        //good
+                        if(reqC[i][0] == 1){
+                            //take once
+                            reqC.splice(i, 1);
+                        }
+                        else if(reqC[i][0] == -3){
+                            reqC[i][0] = 1 //-3 means can be retaken for credit
+                        }
+                        else if(reqC[i][0] == -1){
+                            //do nothing
+                        }
+                        return;
+                    }
+                }
                 if(reqC[i][j] === courseInfoMap["department"]+" "+ courseInfoMap["courseNum"]){
                     flag = 1;
                     //delete that degReq
-                    reqC.splice(i, 1);
+                    if(reqC[i][0] == 1){
+                        //take once
+                        reqC.splice(i, 1);
+                    }
+                    else if(reqC[i][0] == -3){
+                        reqC[i][0] = 1 //-3 means can be retaken for credit
+                    }
+                    else if(reqC[i][0] == -1){
+                        //do nothing
+                    }
                     return;
                 }
             }
@@ -200,15 +258,81 @@ class coursePlan extends Component {
         }
         var electiveC = this.degReqs["Elective Courses"];
         if(flag === 0){
-            
+            for(var i = 0; i < electiveC.length; i++){
+                //check if course name + number matches
+                for(var j = 1; j < electiveC[i].length; j++){
+                    if(electiveC[i][j].substring(0,3) !== courseInfoMap["department"]){
+                        continue;
+                    }
+                    if(electiveC[i][j].includes("-")){
+                        var str = electiveC[i][j].match("(\d{3})\b\s*-\s*\b(\d{3})");
+                        if(str == null){
+                            continue;
+                        }
+                        var match1 = parseInt(str[1]); //get lower class #
+                        var match2 = parseInt(str[2]); //get higher class #
+                        var courseNum = parseInt(courseInfoMap["courseNum"]);
+                        if(courseNum > match1 && courseNum < match2){
+                            //good
+                            if(electiveC[i][0] == 1){
+                                //take once
+                                electiveC.splice(i, 1);
+                            }
+                            else if(electiveC[i][0] == -3){
+                                electiveC[i][0] = 1 //-3 means can be retaken for credit
+                            }
+                            else if(electiveC[i][0] == -1){
+                                //do nothing
+                            }
+                            else if(electiveC[i][0] > 1){
+                                electiveC[i][0] -= 1; //[4, "AMS 542­ - 556"]
+                            }
+                            return;
+                        }
+                    }
+                    if(electiveC[i][j] === courseInfoMap["department"]+" "+ courseInfoMap["courseNum"]){
+                        //delete that degReq
+                        if(electiveC[i][0] == 1){
+                            //take once
+                            electiveC.splice(i, 1);
+                        }
+                        else if(electiveC[i][0] == -3){
+                            electiveC[i][0] = 1 //-3 means can be retaken for credit
+                        }
+                        else if(electiveC[i][0] == -1){
+                            //do nothing
+                        }
+                        else if(electiveC[i][0] > 1){
+                            electiveC[i][0] -= 1; //[4, "AMS 530"]
+                        }
+                        return;
+                    }
+                    else if(electiveC[i][j] === courseInfoMap["department"]){
+                        if(electiveC[i][0] == 1){
+                            //take once
+                            electiveC.splice(i, 1);
+                        }
+                        else if(electiveC[i][0] == -3){
+                            electiveC[i][0] = 1 //-3 means can be retaken for credit
+                        }
+                        else if(electiveC[i][0] == -1){
+                            //do nothing
+                        }
+                        else if(electiveC[i][0] > 1){
+                            electiveC[i][0] -= 1; //[4, "AMS"]
+                        }
+                        return;
+                    }
+                }
+            }
         }
-        var electiveC = this.degReqs["Elective Courses"];
-        // if(flag == 0){
-        // }
     }
+
+
 
     //check if degree requirements are met
     //TODO what if the student has a plan already
+    //looks at degReqs, which is a copy of the student's deg requirements
     degreeReqsSatisfied(){
         //check the degReqs object and see if the req and elective coures is empty
         if(this.degReqs["Required Courses"] === [] && this.degReqs["Elective Courses"] === []){
@@ -218,19 +342,19 @@ class coursePlan extends Component {
     }
 
     retrieveCourseInfo = async function (className) {
-        var courseInfo = undefined;
+        var courseInfo = null;
         className = className.replace(" ", "");
-        // var arr = className.split(" ");
-        // var dep = arr[0];
-        // var idNumber = arr[1];
         await axios
-            .get("http://localhost:5000"+ "/get/course/"+className+"/"+this.sem+"/"+this.year)
+            .get("http://localhost:5000"+ "/courses/get/course/"+className+"/"+this.sem+"/"+this.year)
             .then((course) => courseInfo = course)
-            .catch((err) => console.log("course error: ", err));
-        if(courseInfo === undefined || courseInfo === []){//if undefined or doesn't exist
+            .catch((err) => console.log("course error: ", err), courseInfo = undefined);
+        
+        console.log("retrieveCourseInfo: ", courseInfo["data"]);
+        if(courseInfo === undefined || courseInfo["data"].length === 0){//if undefined or doesn't exist
+            console.log("No courses found");
             return null;
         }
-        return courseInfo;        
+        return courseInfo;
     }
 
     //info = course information from db
